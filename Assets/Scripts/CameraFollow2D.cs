96edc9a3c95c1754e4b57024a0567c96756943bf
Andy Jian <jia.jian@mail.mcgill.ca>
2020-10-14 15:56:50 -0700

    downgrade to mojave

---
+++ Assets/Scripts/CameraFollow2D.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2D : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    [SerializeField] private float timeOffset;
+
+    [SerializeField] private Vector2 posOffset;
+
+    private Vector3 velocity;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 startPos = transform.position;
+        Vector3 endPos = player.transform.position;
+        endPos.x += posOffset.x;
+        endPos.y += posOffset.y;
+        endPos.z = -10;
+
+        //transform.position = Vector3.Lerp(startPos, endPos, timeOffset * Time.deltaTime);
+        //transform.position = new Vector3(playerPos.x, playerPos.y, -10);
+
+        transform.position = Vector3.SmoothDamp(startPos, endPos, ref velocity, timeOffset);
+    }
+}
--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -1,57 +1,124 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController2D : MonoBehaviour
 {
-    Animator animator;
-    Rigidbody2D rb2d;
-    SpriteRenderer spriteRenderer;
+    private Animator animator;
+    private Rigidbody2D rb2d;
+    private SpriteRenderer spriteRenderer;
+    private Collider2D collider;
 
-    bool isGrounded;
+    private bool isGrounded;
+    private bool jump;
+    private enum State { idle, running, jumping, falling, hurt }
+    private State state = State.idle;
 
-    [SerializeField]
-    Transform groundCheck;
-    // Start is called before the first frame update
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private Text cherryText;
+    [SerializeField] private float speed = 1.5f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private int cherries = 0;
+
     void Start()
     {
         animator = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        collider = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        if (collider.IsTouchingLayers(ground))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            jump = true;
+        }
+
+        AnimationState();
+        animator.SetInteger("state", (int) state);
     }
 
     private void FixedUpdate()
     {
-        if(Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")))
-        {
-            isGrounded = true;
-        } else
-        {
-            isGrounded = true;
-        }
+        float hDirection = Input.GetAxis("Horizontal");
 
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        if (hDirection < 0)
         {
-            rb2d.velocity = new Vector2(2, rb2d.velocity.y);
-            animator.Play("Player_run");
-            spriteRenderer.flipX = false;
-        }
-        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb2d.velocity = new Vector2(-2, rb2d.velocity.y);
-            animator.Play("Player_run");
+            rb2d.velocity = new Vector2(-speed, rb2d.velocity.y);
             spriteRenderer.flipX = true;
+        }
+        else if (hDirection > 0)
+        {
+            rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+            spriteRenderer.flipX = false;
         }
         else
         {
-            animator.Play("Player_idle");
             rb2d.velocity = new Vector2(0, rb2d.velocity.y);
         }
 
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        if (jump)
         {
-            rb2d.velocity = new Vector2(rb2d.velocity.x, 3);
-            animator.Play("Player_jump");
+            jump = false;
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+            state = State.jumping;
+        }
+    }
+
+    private void AnimationState()
+    {
+
+        if (state == State.jumping)
+        {
+            if(rb2d.velocity.y < 0.1f)
+            {
+                state = State.falling;
+            }
+        }
+        else if(state == State.falling)
+        {
+            if(isGrounded)
+            {
+                state = State.idle;
+            }
+        }
+        else if(Mathf.Abs(rb2d.velocity.x) > Mathf.Epsilon)
+        {
+            state = State.running;
+        }
+        else
+        {
+            state = State.idle;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Collectible")
+        {
+            Destroy(collision.gameObject);
+            cherries++;
+            cherryText.text = cherries.ToString();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.otherCollider.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
         }
     }
 }
